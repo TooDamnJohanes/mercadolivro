a659a4485224e85a318fd0d21e5f28d06988b362
Joao Careta <j.otavio.careta@gmail.com>
2023-12-29 13:01:44 -0300

    Creating backend project

--- src/main/kotlin/com/example/udemymercadolivro/repository/CustomerRepository.kt
+++ src/main/kotlin/com/example/udemymercadolivro/repository/CustomerRepository.kt
@@ -3,5 +3,4 @@ package com.example.udemymercadolivro.repository
 import com.example.udemymercadolivro.model.Customer
 import org.springframework.data.repository.CrudRepository
 
-interface CustomerRepository: CrudRepository<Customer, Int> {
-}
+interface CustomerRepository: CrudRepository<Customer, Int>
